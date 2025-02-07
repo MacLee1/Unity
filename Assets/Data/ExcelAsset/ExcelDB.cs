98e4379a9c58ffd2d4a5119479c0bbdd84deda75
MacLee1 <biggun230@gmail.com>
2025-02-07 14:02:05 +0900

    Add files via upload

---
+++ Assets/Data/ExcelAsset/ExcelDB.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExcelDB<T> : ScriptableObject
+	where T : class
+{
+	static T _instance;
+
+	public static T Instance
+	{
+		get
+		{
+			if(_instance == null)
+				_instance = Resources.Load("SheetData/" + typeof(T), typeof(T)) as T;
+			return _instance;
+		}
+	}
+}
